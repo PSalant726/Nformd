82203763cd3ca9852abce944aef4dd378615ee56
Phil Salant <PSalant@gmail.com>
2016-08-03 21:18:24 -0400

    Change package.json for use on home machine, add TestUser seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+User.create(username: "TestUser", password: "password", email: "Test@test.com")
