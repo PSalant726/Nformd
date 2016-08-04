4d6444d57ed951f8e84b7a9302fb5505bceada77
Phil Salant <PSalant@gmail.com>
2016-08-04 14:47:35 -0400

    Add Story model, StoriesController, seed db with Faker data

---
+++ app/models/story.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  body       :text             not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Story < ActiveRecord::Base
+  validates :title, :body, :author_id, presence: true
+
+  belongs_to(
+    :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+  )
+  
+end
