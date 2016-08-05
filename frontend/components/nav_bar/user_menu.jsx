f05764409139ccbc896eab2af8125b100f5bb8b7
Phil Salant <PSalant@gmail.com>
2016-08-05 14:39:43 -0400

    Style User menu

--- frontend/components/nav_bar/user_menu.jsx
+++ frontend/components/nav_bar/user_menu.jsx
@@ -16,17 +16,53 @@ const UserMenu = React.createClass({
           <div className="user-menu-inner">
             <ul className="user-menu-content">
 
-              <li>
+              <a>
+                <li className="user-menu-link">New story</li>
+              </a>
 
-              </li>
+              <a>
+                <li className="user-menu-link">Drafts</li>
+              </a>
 
-              <li>
-                <input
-                  className="log-out-button"
-                  type="submit"
-                  value="Log Out"
-                  onClick={ this.handleLogout } />
-              </li>
+              <a>
+                <li className="user-menu-link">Stories</li>
+              </a>
+
+              <a>
+                <li className="user-menu-link">Stats</li>
+              </a>
+
+              <li className="user-menu-list-separator" />
+
+              <a>
+                <li className="user-menu-link">Bookmarks</li>
+              </a>
+
+              <a>
+                <li className="user-menu-link">Publications</li>
+              </a>
+
+              <a>
+                <li className="user-menu-link">Follow your interests</li>
+              </a>
+
+              <li className="user-menu-list-separator" />
+
+              <a className="rem-pad">
+                <li className="user-menu-link personal-link">Profile</li>
+              </a>
+
+              <a className="rem-pad">
+                <li className="user-menu-link personal-link">Settings</li>
+              </a>
+
+              <a className="rem-pad">
+                <li
+                  className="user-menu-link personal-link"
+                  onClick={ this.handleLogout }>
+                  Log Out
+                </li>
+              </a>
 
             </ul>
           </div>
