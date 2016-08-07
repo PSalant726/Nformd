7a5ef2c48cb91db82ba9c1f4fc98a50482aad82a
Phil Salant <PSalant@gmail.com>
2016-08-07 01:32:32 -0400

    Begin styling StoryShow component, update README

--- frontend/nformd.jsx
+++ frontend/nformd.jsx
@@ -3,6 +3,7 @@ const ReactDOM = require('react-dom');
 const App = require('./components/app');
 const SessionActions = require('./actions/session_actions');
 const StoriesIndex = require('./components/stories/stories_index');
+const StoryShow = require('./components/stories/story_show');
 const Modal = require('react-modal');
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
@@ -10,6 +11,8 @@ const appRouter = (
   <Router history={ hashHistory }>
     <Route path="/" component={ App }>
       <IndexRoute component={ StoriesIndex } />
+      <Route path="stories/" component={ StoriesIndex } />
+      <Route path="stories/:id" component={ StoryShow } />
     </Route>
   </Router>
 );
