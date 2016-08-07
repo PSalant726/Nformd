466ab518288878c83ffc012fe5b66c4ea35e8dd0
Phil Salant <PSalant@gmail.com>
2016-08-07 19:58:45 -0400

    Add Story creation page, style nav bar, build publish menu

---
+++ frontend/components/nav_bar/publish_menu.jsx
@@ -0,0 +1,40 @@
+const React = require('react');
+
+const PublishMenu = React.createClass({
+  publishStory(){},
+
+  menuState(){
+    if(this.props.menuVisible){
+      return(
+        <div className="publish-menu-outer">
+          <div className="publish-menu-arrow" />
+          <div className="publish-menu-inner">
+            <ul className="publish-menu-content">
+              <li className="publish-add-tags">
+                <h1>Ready to publish?</h1>
+                <p>Add or change tags (up to 5) so your story reaches more people:</p>
+                <div className="add-tags-placeholder" />
+              </li>
+              <li className="publish-menu-list-separator" />
+              <li className="publish-button-container">
+                <button
+                  className="publish-button"
+                  onClick={ this.publishStory }>
+                  Publish
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  },
+
+  render(){
+    return(<div>{ this.menuState() }</div>);
+  }
+});
+
+module.exports = PublishMenu;
