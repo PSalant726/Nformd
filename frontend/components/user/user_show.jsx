1d99078c6e8467e6001012e8efc2f9fe31aaa2a0
Phil Salant <PSalant@gmail.com>
2016-08-09 00:57:42 -0400

    Add user_show elements

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const UserStore = require('../../stores/user_store');
 const UserActions = require('../../actions/user_actions');
+const Link = require('react-router').Link;
 
 const UserShow = React.createClass({
   getInitialState(){
@@ -20,9 +21,29 @@ const UserShow = React.createClass({
     this.setState({ user: UserStore.user() });
   },
 
+  followToggle(){
+    // TODO: Render and return Follow/Following button
+  },
+
   render(){
     return(
-      <div>User Show Page!</div>
+      <div className="user-details">
+        <h1 className="user-username">{ this.state.user.username }</h1>
+        <p className="user-bio">{ this.state.user.bio }</p>
+        <div className="user-show-avatar-placeholder">
+          User Profile Image
+        </div>
+        <p className="user-num-following">100 Following</p>
+        <p className="user-num-followers">50 Followers</p>
+        { this.followToggle() }
+        <ul className="user-feed-tabs">
+          <li>Profile</li>
+          <li>Latest</li>
+          <li>Recommends</li>
+          <li>Responses</li>
+        </ul>
+        <div className="user-feed-goes-here">User Feed!</div>
+      </div>
     );
   }
 });
