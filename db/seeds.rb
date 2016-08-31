77c01b795f09e88dcc3fcddab7e1cadc85ccd32a
Phil Salant <PSalant@gmail.com>
2016-08-31 17:41:35 -0400

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -130,8 +130,22 @@ Story.create(
   author_id: User.where.not(username: "guest.user").sample.id
 )
 
+# NB: Add instructional story at the top of the feed:
+
+Story.create(
+  title: "Want to create stories of your own? Sign in as a guest!",
+  body: "The guest user can respond to articles by others, and recommend articles too!",
+  author_id: User.find_by(username: "guest.user").id
+)
+
 # Seed Responses
 Comment.delete_all
+
+Comment.create(
+  story_id: Story.find_by(title: "Want to create stories of your own? Sign in as a guest!").id,
+  author_id: User.find_by(username: "guest.user").id,
+  body: "You can respond to your own stories too!"
+)
 
 comment_bodies = [
   "Wow, what a great article!",
@@ -148,7 +162,7 @@ comment_bodies = [
 
 50.times do
   Comment.create(
-    story_id: Story.all.sample.id,
+    story_id: Story.where.not(title: "Want to create stories of your own? Sign in as a guest!").sample.id,
     author_id: User.where.not(username: "guest.user").sample.id,
     body: comment_bodies.sample
   )
@@ -170,22 +184,8 @@ Recommend.delete_all
 
 30.times do
   recommend = Recommend.new(
-    author_id: User.all.sample.id,
-    story_id: Story.all.sample.id
+    author_id: User.where.not(username: "guest.user").sample.id,
+    story_id: Story.where.not(title: "Want to create stories of your own? Sign in as a guest!").sample.id
   )
   redo unless recommend.save
 end
-
-# NB: Add instructional story at the top of the feed:
-
-Story.create(
-  title: "Want to create stories of your own? Sign in as a guest!",
-  body: "The guest user can respond to articles by others, and recommend articles too!",
-  author_id: User.find_by(username: "guest.user").id
-)
-
-Comment.create(
-  story_id: Story.find_by(title: "Want to create stories of your own? Sign in as a guest!").id,
-  author_id: User.find_by(username: "guest.user").id,
-  body: "You can respond to your own stories too!"
-)
